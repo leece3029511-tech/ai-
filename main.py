603c5d9ad5f671b5ee675888328008246cabba42
leece3029511-tech <leece3029511@gmail.com>
2026-05-07 15:18:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('나의 첫 웹서브스 만들기!')
 a=st.text_input('이름을 입력해주새요!')
 b=st.selectbox('좋아하는 축구선수를 선택해주세요!',['호날두','메시','네이마르','그리즈만','호나우지뉴','호나우두','펠레','마라도나'])
-if st.button('인사말 생성')
+if st.button('인사말 생성'):
  st.write(a+'님!,안녕하세요,반갑습니다!')
